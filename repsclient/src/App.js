13b3da932f25fbbf2ce149e001b3fe876eb0e0a7
Mathias Boampong <mathiasboampong123@gmail.com>
2020-10-08 16:31:30 +0000

    done

---
+++ repsclient/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>
+        <LandingPage />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
